4b503900ca447501a91e26a6ee31b3714bd4672e
Sam Minot <sam@fredhutch.org>
2018-11-27 10:54:23 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # docker-fastqc
 Docker image running FASTQC
+
+Image contains FastQC as well as a wrapper script to make it easy to run on files in AWS S3
+
+### Wrapper script
+
+`run_fastqc.sh`
+
+Executable script is in the PATH
+
+Arguments are <INPUT_FASTQ> and <OUTPUT>
+
+<OUTPUT> can be a file or a folder on S3
+
+The output file is in ZIP format.
+
+Example:
+
+```
+run_fastqc.sh s3://bucket-name/folder1/reads/file.fastq.gz s3://bucket-name/folder1/fastqc/file.fastqc.zip
+```
---
+++ run_fastqc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Make sure the output key is reasonable
+echo Output location is $2
+aws s3 ls $2
+
+# Download input file from S3
+echo Downloading input file from S3
+echo Provided input path is $1
+
+aws s3 cp "$1" /share
+
+# Get the filename
+filename=${1##*/}
+echo Filename is $filename
+
+# Make sure the file exists
+[[ -s /share/$filename ]]
+
+# Run FastQC
+echo Running FastQC
+fastqc -f fastq /share/$filename
+
+# Return the output
+aws s3 cp $(find /share -name "*zip") $2
+
+# Clean up temp files
+echo Removing temporary files
+rm /share/$filename $(find /share -name "*zip")
+
+echo Done
